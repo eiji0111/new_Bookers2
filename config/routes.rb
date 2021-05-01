bffae693037e8b94fd70fae0aef2ec776404c54e
eiji0111 <ee111fender@gmail.com>
2021-05-01 13:03:55 +0000

    [Add]いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
