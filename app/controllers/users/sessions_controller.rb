03f081a2ef531c932f12fa778ab438f19521fa49
eiji0111 <ee111fender@gmail.com>
2021-05-08 05:03:23 +0000

    [Fix]退会機能再ログイン修正

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Users::SessionsController < Devise::SessionsController
+  before_action :reject_inactive_user, only: [:create]
+　
+  def reject_inactive_user
+    @user = User.find_by(name: params[:user][:name])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && @user.is_valid == false
+        flash[:notice] = "退会済みのユーザーです"
+        redirect_to new_user_session_path
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations", sessions: "users/sessions"
   }
   resources :users, only: [:index, :show, :edit, :update]
   resources :books do
